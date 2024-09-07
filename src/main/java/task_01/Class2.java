33e187b029274b196e38e68ac2ed2acb6bfb00a3
NikolayEmrich <emrich.work@gmail.com>
2024-09-07 13:59:44 +0200

    task_01 (with changes)

--- src/main/java/task_01/Class2.java
+++ src/main/java/task_01/Class2.java
@@ -4,7 +4,7 @@ public class Class2 {
 
     public static void main(String[] args) {
 
-        System.out.println("Решение задачи 2");
+        System.out.println("Решение задачи 2. С правками");
 
     }
 }
